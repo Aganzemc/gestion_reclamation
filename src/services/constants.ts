1e1e4b3206fd80e03919ff281acca021f21232b2
Jospin <jospinndagano1@gmail.com>
2025-09-11 00:59:29 +0200

    add the right api url

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -1,4 +1,4 @@
 
 
-export const prodUrl = "https://gestionreclamationsbackend.vercel.app"
+export const prodUrl = "https://gestion-reclamations-backend.vercel.app"
 export const devUrl = "http://localhost:4000"
